df14bf7558a3e6a0dfc405d36852b44fab5d9e69
ayush-bothra <ayush9bothra@gmail.com>
2025-08-20 12:35:00 +0000

    chose mongoDB, wrote reqirements

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,13 +5,25 @@ package db
 handle SQlite3 /  MongoDB (preferred)
 
 imports:
-MongoDB imports (whichever one is needed, will search)
+MongoDB imports :
+"context"
+"time"
+"go.mongodb.org/mongo-driver/mongo"
+"go.mongodb.org/mongo-driver/mongo/options"
+"go.mongodb.org/mongo-driver/bson"
 utils package for the logging/config
 
 functions:
 
 initDB(path string) (*sql.DB, error)
 CloseDB(db *sql.DB) to close the DB
-
+other required mongoDB specific functions
+ConnectMongo(uri string) (*mongo.Client, error)
+InsertDocument(ctx context.Context, collection string, document interface{}) (*mongo.InsertOneResult, error)
+FindOneDocument(ctx context.Context, collection string, filter interface{}, result interface{}) error
+FindManyDocuments(ctx context.Context, collection string, filter interface{}, results interface{}) error
+UpdateDocument(ctx context.Context, collection string, filter interface{}, update interface{}) (*mongo.UpdateResult, error)
+DeleteDocument(ctx context.Context, collection string, filter interface{}) (*mongo.DeleteResult, error)
+(for CRUD operations)
 another file called models.go may be required
 */
