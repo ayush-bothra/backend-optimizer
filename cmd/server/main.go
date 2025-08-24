792e69e682f49e7f8ebddc7707e5e04ac9b3ce4e
ayush-bothra <ayush9bothra@gmail.com>
2025-08-24 14:54:17 +0000

    added basic HTTP handling and CRUD operations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,3 +15,62 @@ need fiber/v2 for the HTTP
 functions required: 
 main
 */
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/ayush-bothra/backend-optimizer/internal/db"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"context"
+
+)
+
+
+func helloHandler(wrt http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(wrt, "Hello World!\n")
+}
+
+func main() {
+	
+	if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    mongoURI := os.Getenv("MONGODB_URI")
+    if mongoURI == "" {
+        log.Fatal("MONGODB_URI is not set")
+    }
+
+    DB, client := db.ConnectDB(mongoURI)
+
+	// This function will run after connect
+	// is done running, similar to the 
+	// destructor (other sections of the code will function as normal)
+	// this one here will disconnect from the db
+	// and if there is a problem, it will PANIK
+	defer func() {
+	if err := client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+	}()
+
+	todo := db.ToDoList_DB{Title: "Learn mongoDB", Done: false}
+	res, err := db.InsertTodo(DB, todo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Inserted ID: ", res.InsertedID)
+
+	// HandleFunc registers the handler function for the given pattern in [DefaultServeMux].
+	// The documentation for [ServeMux] explains how patterns are matched.
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("server running at http://localhost:8080/")
+
+	// ListenAndServe listens on the TCP network address addr and then calls
+	// [Serve] with handler to handle requests on incoming connections.
+	// The handler is typically nil, in which case [DefaultServeMux] is used.
+	http.ListenAndServe(":8080", nil)
+}
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,5 @@
 package db
 
-
 /*
 handle SQlite3 /  MongoDB (preferred)
 
@@ -21,9 +20,87 @@ other required mongoDB specific functions
 ConnectMongo(uri string) (*mongo.Client, error)
 InsertDocument(ctx context.Context, collection string, document interface{}) (*mongo.InsertOneResult, error)
 FindOneDocument(ctx context.Context, collection string, filter interface{}, result interface{}) error
-FindManyDocuments(ctx context.Context, collection string, filter interface{}, results interface{}) error
+FindManyDocuments(ctx context.Context, collection string, filter interface{}, results interface{}) error *(NOT DONE)
 UpdateDocument(ctx context.Context, collection string, filter interface{}, update interface{}) (*mongo.UpdateResult, error)
 DeleteDocument(ctx context.Context, collection string, filter interface{}) (*mongo.DeleteResult, error)
 (for CRUD operations)
 another file called models.go may be required
 */
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+)
+
+// for typed IDs, use bson.ObjectID
+// here any is fine, so we will use it
+// any is similar to auto_inc from mysql
+type ToDoList_DB struct {
+	ID any `bson:"_id,omitempty"`
+	Title string `bson:"title"`
+	Done bool `bson:"done"`
+}
+
+func ConnectDB(uri string) (*mongo.Database, *mongo.Client) {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	// Client creates a new ClientOptions instance.
+	// ApplyURI parses the given URI and sets options accordingly.
+	// ApplyURI is a reciever (obj.method type) on *ClientOptions
+	// SetServerAPIOptions specifies a ServerAPIOptions instance 
+	// used to configure the API version sent to the server
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(opts)
+	if err != nil {
+	panic(err)
+	}
+
+	// Send a ping to confirm a successful connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	return client.Database("Todo_DB"), client
+}
+
+func InsertTodo (db *mongo.Database, todo ToDoList_DB) (*mongo.InsertOneResult, error) {
+	collection := db.Collection("Todo_DB")
+	return collection.InsertOne(context.TODO(), todo)
+} 
+
+func GetTodoByID (db *mongo.Database, id any) (ToDoList_DB, error) {
+	collection := db.Collection("Todo_DB")
+	var result ToDoList_DB
+	filter := bson.M{"_id":id}
+
+	// FindOne executes a find command and returns a SingleResult for one document in the collection.
+	// The filter parameter must be a document containing query operators and can be used to select the document to be
+	// returned. It cannot be nil.
+	// Decode will unmarshal the document represented by this SingleResult into v.
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+	return result, err
+}
+
+func UpdateTodo (db *mongo.Database, id any, update_value ToDoList_DB) (*mongo.UpdateResult, error) {
+	collection := db.Collection("Todo_DB")
+	filter := bson.M{"_id":id}
+	update := bson.M{"$set":update_value}
+
+	// UpdateOne executes an update command to update at most one document in the collection.
+	// The update parameter must be a document containing update operators
+	return collection.UpdateOne(context.TODO(), filter, update)
+}
+
+func DeleteTodo (db *mongo.Database, id any) (*mongo.DeleteResult, error) {
+	collection := db.Collection("Todo_DB")
+	filter := bson.M{"_id":id}
+	return collection.DeleteOne(context.TODO(), filter)
+}
